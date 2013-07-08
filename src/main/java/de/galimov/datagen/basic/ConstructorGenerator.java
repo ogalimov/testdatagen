30835bb8ec3022d9dc71545f23bc64b74a044402
Oleg Galimov <galimov@gmx.de>
2013-07-08 22:40:39 +0200

    ConstructorGenerator now matches by number of arguments first.

--- src/main/java/de/galimov/datagen/basic/ConstructorGenerator.java
+++ src/main/java/de/galimov/datagen/basic/ConstructorGenerator.java
@@ -1,6 +1,8 @@
 package de.galimov.datagen.basic;
 
 import java.lang.reflect.Constructor;
+import java.util.LinkedList;
+import java.util.List;
 
 import de.galimov.datagen.api.DataGenerator;
 
@@ -23,28 +25,55 @@ public class ConstructorGenerator<T> extends AbstractDataGenerator<T> {
         }
     }
 
+    @SuppressWarnings("unchecked")
     private static <T> Constructor<T> findConstructor(Class<T> clazz, DataGenerator[] parameterGenerators) {
+        List<Constructor<?>> constructors = getConstructorsByNumberOfArguments(clazz, parameterGenerators.length);
+
+        if (constructors.size() == 0) {
+            throw new IllegalArgumentException("No constructor matching the number of parameters found");
+        } else if (constructors.size() == 1) {
+            return (Constructor<T>) constructors.get(0);
+        } else {
+            return findConstructorByClasses(clazz, parameterGenerators);
+        }
+
+
+    }
+
+    private static <T> List<Constructor<?>> getConstructorsByNumberOfArguments(Class<T> clazz, int numberOfArguments) {
+        Constructor<?>[] constructors = clazz.getConstructors();
+        List<Constructor<?>> constructorsWithMatchingNumberOfArguments = new LinkedList<Constructor<?>>();
+        for (Constructor<?> constructor : constructors) {
+            if (constructor.getParameterTypes().length == numberOfArguments) {
+                    constructorsWithMatchingNumberOfArguments.add(constructor);
+            }
+        }
+        return constructorsWithMatchingNumberOfArguments;
+    }
+
+    private static <T> Constructor<T> findConstructorByClasses(Class<T> clazz, DataGenerator[] parameterGenerators) {
         Class[] parameterClasses = new Class[parameterGenerators.length];
 
-        for(int i = 0; i < parameterGenerators.length; i++) {
+        for (int i = 0; i < parameterGenerators.length; i++) {
             parameterClasses[i] = parameterGenerators[i].getGeneratedClass();
 
-            if(parameterClasses[i] == null) {
-                throw new RuntimeException("When using ConstructorGenerator by class, the generatedClass must be set for all parameter generators.");
+            if (parameterClasses[i] == null) {
+                throw new IllegalArgumentException("When using ConstructorGenerator by class, the generatedClass must be set " +
+                        "for all parameter generators, if there is more than one constructor with the given number of arguments");
             }
         }
 
         try {
             return clazz.getConstructor(parameterClasses);
         } catch (NoSuchMethodException e) {
-            throw new RuntimeException("No constructor matching the signature of the parameter generators found", e);
+            throw new IllegalArgumentException("No constructor matching the signature of the parameter generators found", e);
         }
     }
 
     @Override
     protected T generateInternal() {
         Object[] parameters = new Object[parameterGenerators.length];
-        for(int i = 0; i < parameterGenerators.length; i++) {
+        for (int i = 0; i < parameterGenerators.length; i++) {
             parameters[i] = parameterGenerators[i].getValue();
         }
 
--- src/test/java/de/galimov/datagen/basic/ConstructorGeneratorTest.java
+++ src/test/java/de/galimov/datagen/basic/ConstructorGeneratorTest.java
@@ -11,6 +11,16 @@ public class ConstructorGeneratorTest {
         private final int x;
         private final long y;
 
+        public TestClass(int x) {
+            this.x = x;
+            y = 0;
+        }
+
+        public TestClass(int x, int y) {
+            this.x = x;
+            this.y = y + 100;
+        }
+
         public TestClass(int x, long y) {
             this.x = x;
             this.y = y;
@@ -38,11 +48,19 @@ public class ConstructorGeneratorTest {
     }
 
     @Test
-    public void testConstructorGeneration() {
+    public void testConstructorGenerationByTypes() {
         DataGenerator<Integer> generator1 = new ConstantGenerator<Integer>(int.class, 3);
         DataGenerator<Long> generator2 = new ConstantGenerator<Long>(long.class, 3L);
         ConstructorGenerator<TestClass> constructorGenerator = new ConstructorGenerator<TestClass>(TestClass.class, generator1, generator2);
         TestClass result = constructorGenerator.generate();
         assertEquals(new TestClass(3, 3L), result);
+    }
+
+    @Test
+    public void testConstructorGenerationByNumberOfArguments() {
+        DataGenerator<Integer> generator1 = new ConstantGenerator<Integer>(Integer.class, 3);
+        ConstructorGenerator<TestClass> constructorGenerator = new ConstructorGenerator<TestClass>(TestClass.class, generator1);
+        TestClass result = constructorGenerator.generate();
+        assertEquals(new TestClass(3), result);
     }
 }
