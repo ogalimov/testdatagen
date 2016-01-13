27f430b2b60c147e9f180ff83ee48f596a39d815
Oleg Galimov <oleg.galimov@baaderbank.de>
2016-01-13 12:44:04 +0100

    Added Method to RandomAsciiStringGenerator to be able to exclude Characters

--- src/main/java/de/galimov/datagen/random/RandomAsciiStringGenerator.java
+++ src/main/java/de/galimov/datagen/random/RandomAsciiStringGenerator.java
@@ -2,11 +2,16 @@ package de.galimov.datagen.random;
 
 import de.galimov.datagen.api.DataGenerator;
 
+import java.util.Collection;
+import java.util.Collections;
+
 import static de.galimov.datagen.api.Generation.constant;
 
 public class RandomAsciiStringGenerator extends AbstractRngDataGenerator<String> {
     private final DataGenerator<Integer> minNumChars;
     private final DataGenerator<Integer> maxNumChars;
+
+    private final Collection<Character> forbiddenLetters;
 
     private static final int MIN_LETTER = 32;
     private static final int MAX_LETTER = 127;
@@ -16,8 +21,18 @@ public class RandomAsciiStringGenerator extends AbstractRngDataGenerator<String>
      * @param maxNumChars maximum Length of the generated String
      */
     public RandomAsciiStringGenerator(DataGenerator<Integer> minNumChars, DataGenerator<Integer> maxNumChars) {
+        this(minNumChars, maxNumChars, Collections.emptyList());
+    }
+
+    public RandomAsciiStringGenerator(Integer minNumChars, Integer maxNumChars) {
+        this(constant(minNumChars), constant(maxNumChars));
+    }
+
+    public RandomAsciiStringGenerator(DataGenerator<Integer> minNumChars, DataGenerator<Integer> maxNumChars, Collection<Character> forbiddenLetters) {
         this.minNumChars = minNumChars;
         this.maxNumChars = maxNumChars;
+
+        this.forbiddenLetters = forbiddenLetters;
 
         registerChildGenerator(minNumChars);
         registerChildGenerator(maxNumChars);
@@ -25,8 +40,8 @@ public class RandomAsciiStringGenerator extends AbstractRngDataGenerator<String>
         setGeneratedClass(Integer.class);
     }
 
-    public RandomAsciiStringGenerator(Integer minNumChars, Integer maxNumChars) {
-        this(constant(minNumChars), constant(maxNumChars));
+    public RandomAsciiStringGenerator(Integer minNumChars, Integer maxNumChars, Collection<Character> forbiddenLetters) {
+        this(constant(minNumChars), constant(maxNumChars), forbiddenLetters);
     }
 
     @Override
@@ -40,7 +55,11 @@ public class RandomAsciiStringGenerator extends AbstractRngDataGenerator<String>
         StringBuilder result = new StringBuilder();
 
         for (int i = 0; i < length; i++) {
-            result.append(((char) (getRandom().nextInt(MAX_LETTER - MIN_LETTER) + MIN_LETTER)));
+            char randomChar;
+            do {
+                randomChar = (char) (getRandom().nextInt(MAX_LETTER - MIN_LETTER) + MIN_LETTER);
+            } while (forbiddenLetters.contains(randomChar));
+            result.append(randomChar);
         }
 
         return result.toString();
